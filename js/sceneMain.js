afebc91709ac35b77af15fc2279b79e55e6bb5d5
placidohoff <sweetflow401@gmail.com>
2020-07-11 21:46:39 -0400

    Modifying for Align Variables needed

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -7,15 +7,6 @@ class SceneMain extends Phaser.Scene {
         
     }
     create(){
-        emitter = new Phaser.Events.EventEmitter();      
-        controller = new Controller();
-
-        this.sb = new ScoreBox({scene: this});
-        this.sb.x = game.config.width /2;
-        this.sb.y = 50; 
-
-        //model.score = 100;
-        console.log("Hello There..");
    
     }
     update(){
